901707d673f92887180a0d60f762db0aa0de83cf
MNGiit <36828920+MNGiit@users.noreply.github.com>
2022-09-06 09:32:39 -0400

    create selectTarget()

---
+++ app.js
@@ -0,0 +1,12 @@
+// start() starts combat round, player 1 goes first
+// selectCard()
+// selectTarget()
+const selectTarget = () => {
+    console.log("Target was selected.");
+    // card effect
+    // checkHealth();
+}
+
+// checkHealth() on all. if all player health is gone, game over, player loses
+// checkHealth() if all enemies health are gone, game over, player wins
+// checkHealth() if both player and enemy has health, continue
