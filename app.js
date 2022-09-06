f55d72d711a31772229682e17afbb773a037dab7
MNGiit <36828920+MNGiit@users.noreply.github.com>
2022-09-06 09:50:47 -0400

    selectTarget() is able to get select onclick

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+let selected;
+
 // start() starts combat round, player 1 goes first
 // selectCard()
 // selectTarget()
-const selectTarget = () => {
+const selectTarget = (s) => {
+    // selected = this; // seems to get the whole window
+    // console.log(selected.event);
+    selected = s;
     console.log("Target was selected.");
+    console.log("selected:", selected)
+
+
+    
     // card effect
     // checkHealth();
+
+
 }
 
 // checkHealth() on all. if all player health is gone, game over, player loses
